cdfe3deebfb7ecd8dc08fa967f683a444b0c7a37
newbalancem5 <newbalancem5@gmail.com>
2021-09-08 21:51:47 +0300

    create new file with text for app

---
+++ lib/const/_text.dart
@@ -0,0 +1,5 @@
+String callme = 'Связаться';
+String contactme =
+    'Если вы хотите разместить рекламу, вакансию или анонс то пишите мне.';
+String socialme = 'Где нас можно почитать.';
+String ffdtext = 'Flutter For Dev - самое интересное в мире Flutter.';
