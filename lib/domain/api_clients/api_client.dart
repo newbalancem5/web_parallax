53fafabccab4b5d15abaaefd71dddb3ddeba09be
newbalancem5 <newbalancem5@gmail.com>
2021-10-15 12:01:14 +0300

    update

--- lib/domain/api_clients/api_client.dart
+++ lib/domain/api_clients/api_client.dart
@@ -1,27 +1,28 @@
-import 'dart:convert';
-import 'dart:io';
-import 'package:http/http.dart' as http;
-import 'package:flutterfordev/domain/entity/telegraminf.dart';
+// import 'dart:convert';
+// import 'dart:io';
+// import 'package:http/http.dart' as http;
+// import 'package:flutterfordev/domain/entity/telegraminf.dart';
 
-class ApiClient {
-  final client = HttpClient();
-  String key = '634693ccec46e47ec5c564940ea1ef64';
+// class ApiClient {
+//   final client = HttpClient();
+//   String key = '634693ccec46e47ec5c564940ea1ef64';
 
-  Future<List<Post>> getPosts() async {
-    final url = Uri.parse(
-        'https://api.tgstat.ru/channels/stat?token=$key&channelId=@flutterdevru');
+//   Future<List<Post>> getPosts() async {
+//     final url = Uri.parse(
+//         'https://api.tgstat.ru/channels/stat?token=$key&channelId=@flutterdevru');
 
-    final request = await client.getUrl(url);
-    final response = await http.get(url);
-    print(response);
-    // final response = await request.close();
+//     final response = await client.getUrl(url);
+//     // final response = await http.get(url);
+//     print(response);
 
-    final jsonStrings = await request.toString();
-    final jsonString = jsonStrings.length;
-    final json = null;
-    final info = json
-        .map((dynamic e) => Post.fromJson(e as Map<String, dynamic>))
-        .toList();
-    return info;
-  }
-}
+//     final jsonStrings = await response.toString();
+//     final jsonString = jsonStrings.length;
+//     final json = null;
+//     final info = json
+//         .map(
+//           (dynamic e) => Post.fromJson(e as Map<String, dynamic>),
+//         )
+//         .toList();
+//     return info;
+//   }
+// }
