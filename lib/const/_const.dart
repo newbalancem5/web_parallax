842cd726ae97d540691929ad242feb436b27ebc8
newbalancem5 <newbalancem5@gmail.com>
2022-09-03 11:33:50 +0300

    add const for styleText

--- lib/const/_const.dart
+++ lib/const/_const.dart
@@ -1,3 +1,4 @@
 export 'package:flutterfordev/const/_images.dart';
 export 'package:flutterfordev/const/_link.dart';
 export 'package:flutterfordev/const/_text.dart';
+export 'package:flutterfordev/const/_styleText.dart';
---
+++ lib/const/_styleText.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CustomTextStyle {
+  static const customTitle = TextStyle(
+    fontSize: 17,
+  );
+}
