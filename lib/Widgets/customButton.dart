e65025d5761443d9055b2337c98a14b89400e1da
newbalancem5 <newbalancem5@gmail.com>
2022-09-03 17:43:33 +0300

    update custom color
    add for button: buttonFocusColor, splashColorButton, buttonFocusColor, buttonHoverColor, buttonBoxShadow

--- lib/Widgets/customButton.dart
+++ lib/Widgets/customButton.dart
@@ -11,16 +11,24 @@ class CustomButton extends StatefulWidget {
     this.buttonHeight,
     this.buttonWidth,
     this.buttonBoxShadow,
+    this.buttonFocusColor,
+    this.buttonHoverColor,
+    this.splashColorButton,
+    this.buttonShape,
   }) : super(key: key);
 
   final VoidCallback onPressed;
-  final List buttonBoxShadow;
+  final List<BoxShadow> buttonBoxShadow;
+  final Color buttonHoverColor;
   final String buttonText;
   final Color buttonBackgroudColor;
   final BorderRadius buttonBorderRadius;
   final double buttonHeight;
   final double buttonWidth;
   final TextStyle customTextStyle;
+  final Color buttonFocusColor;
+  final Color splashColorButton;
+  final ShapeBorder buttonShape;
 
   @override
   _CustomButtonState createState() => _CustomButtonState();
@@ -31,17 +39,23 @@ class _CustomButtonState extends State<CustomButton> {
   Widget build(BuildContext context) {
     return Container(
       height: widget.buttonHeight,
-      width: widget.buttonHeight,
+      width: widget.buttonWidth,
       decoration: BoxDecoration(
         boxShadow: widget.buttonBoxShadow,
         color: widget.buttonBackgroudColor,
-        borderRadius: widget.buttonBorderRadius ?? BorderRadius.circular(0),
+        borderRadius: widget.buttonBorderRadius,
       ),
       child: MaterialButton(
+        splashColor: widget.splashColorButton ?? Colors.red,
+        shape: widget.buttonShape,
+        focusColor: widget.buttonFocusColor,
+        hoverColor: widget.buttonHoverColor,
         onPressed: widget.onPressed,
-        child: Text(
-          widget.buttonText,
-          style: widget.customTextStyle,
+        child: Container(
+          child: Text(
+            widget.buttonText,
+            style: widget.customTextStyle,
+          ),
         ),
       ),
     );
